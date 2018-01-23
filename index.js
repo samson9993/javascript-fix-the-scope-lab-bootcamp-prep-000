abe6141ae75fbdc57acba3103dd3cbec336b538a
Samson Wong <samson9993@gmail.com>
2018-01-23 20:00:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,10 +10,11 @@ var animal ='cat'
 }
 
 function add2(n) {
-  return n + two
-
+  
   // Feel free to move things around!
   const two = 2
+  return n + two
+
 }
 
 var funkyFunction = function() {
